919bf2d298687a757014498909889ec438b0d9ad
michaelgreenl <lgreener25@gmail.com>
2023-09-16 19:44:39 -0400

    init

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import styles from './styles/page.module.css';
+import Navbar from '@/components/Navbar/Navbar';
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <Navbar />
+      hello
+      <div className='hi'>asdfjasdkfjdskasdflskdfi</div>
+    </main>
+  );
+}
