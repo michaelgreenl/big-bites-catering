ba4b869396bb08f3992d1e619a1281ee64a0e18c
michaelgreenl <lgreener25@gmail.com>
2023-12-09 18:37:54 -0500

    Added icons to metadata data and edited the generateMetadata hook to accommodate for the added object.

--- hooks/generateMetadata.js
+++ hooks/generateMetadata.js
@@ -4,5 +4,10 @@ export async function generateMetadata(page) {
   const file = await fs.readFile(process.cwd() + '/app/_data/data.json', 'utf8');
   const data = JSON.parse(file);
 
-  return data.metadata[`${page}`];
+  return {
+    title: data.metadata[`${page}`].title,
+    description: data.metadata[`${page}`].description,
+    icons: data.metadata.icons,
+    manifest: '/site.webmanifest',
+  };
 }
